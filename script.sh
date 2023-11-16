bb48913c1375a8e661d7aaa1b0d0e4a7d8716ec1
XIA-LIYI <xialiyi@nus.edu.sg>
2023-11-16 15:44:07 +0800

    Modify script

--- script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#SBATCH --time=20
-#SBATCH --partition=standard
-#SBATCH --nodes=8
-#SBATCH --ntasks=8 --cpus-per-task=24
-#SBATCH --ntasks-per-node=1
-#SBATCH --nodelist=xgph0,xgph1,xgph2,xgph3,xgph4,xgph5,xgpe0,xgpe1
-srun -n 8 ./client
