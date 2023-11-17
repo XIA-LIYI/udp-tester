c51c72e0d6d0af534428ef39dcd5a9660fe7e679
XIA-LIYI <xialiyi@nus.edu.sg>
2023-11-17 14:17:56 +0800

    Add script

---
+++ check.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#SBATCH --time=20
+#SBATCH --partition=standard
+#SBATCH --nodes=6
+#SBATCH --ntasks=6 --cpus-per-task=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --nodelist=xgph0,xgph1,xgph2,xgph3,xgph4,xgph5
+srun -n 6 top
+sleep 5
+srun -n 6 top
+sleep 5
