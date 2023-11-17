23f935fe4b96446764e9a907666e6fd71024093b
XIA-LIYI <xialiyi@nus.edu.sg>
2023-11-17 14:51:12 +0800

    Use 2 threads

--- standard.sh
+++ standard.sh
@@ -2,7 +2,7 @@
 #SBATCH --time=20
 #SBATCH --partition=standard
 #SBATCH --nodes=6
-#SBATCH --ntasks=6 --cpus-per-task=1
+#SBATCH --ntasks=6 --cpus-per-task=2
 #SBATCH --ntasks-per-node=1
 #SBATCH --nodelist=xgph0,xgph1,xgph2,xgph3,xgph4,xgph5
 srun -n 6 ./client
